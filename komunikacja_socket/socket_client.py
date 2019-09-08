b33ac918d3ff0fdf858a1942ab917a45bc719199
Marek Czekala <m-czekala@o2.pl>
2019-09-08 07:46:06 +0200

    Ujednolicenie wersji - ta sama co na Rpi

--- komunikacja_socket/socket_client.py
+++ komunikacja_socket/socket_client.py
@@ -18,6 +18,24 @@ def wczytaj_dane_z_pliku_csv(nazwa_pliku_csv, offset ):
 
     return lista_na_dane
 
+def wczytaj_dane_z_pliku_csv_Timestamp(nazwa_pliku_csv, offset ):
+    lista_na_dane = []
+    result = []
+    with open(nazwa_pliku_csv,"r") as plik :
+        licznik_linii = 0
+        for linia in plik:
+            licznik_linii += 1
+            if licznik_linii > offset :
+                linia_tokeny = linia.split(',') # dzielimy stringa po ','
+                wartosci = linia_tokeny[-6:-5] + linia_tokeny[-4:-1] # [-6] Node Timestamp
+                lista_na_dane.append(wartosci)# dane o ksztalcie (shape) np.(1243,3)
+        lista_na_dane.sort() # sortujemy po Timestamp'ie
+        
+        for i in range(len(lista_na_dane)): # usuwamy Timestamp z rezultatu
+            result.append(lista_na_dane[i][1:])
+    return result
+
+
 def usun_ostatnie_N_rekordow(lista,N):
     licznik = 1
     while(licznik <= N):
@@ -37,11 +55,12 @@ def polacz_liste_w_1D(lista):
         result.append(lista[i][0] +" " + lista[i][1]+ " " + lista[i][2])
     return result
 
-lista_akc_bieganie = wczytaj_dane_z_pliku_csv("../dane/przykladowe/Bieg2_Accelerometer.csv",5)
-lista_gyr_bieganie = wczytaj_dane_z_pliku_csv("../dane/przykladowe/Bieg2_Gyroscope.csv",5)
-lista_akc_trucht = wczytaj_dane_z_pliku_csv("../dane/przykladowe/Trucht3_Accelerometer.csv",5)
-lista_gyr_trucht = wczytaj_dane_z_pliku_csv("../dane/przykladowe/Trucht3_Gyroscope.csv",5)
+lista_akc_bieganie = wczytaj_dane_z_pliku_csv_Timestamp("Bieg1_Accelerometer.csv",5)
+lista_gyr_bieganie = wczytaj_dane_z_pliku_csv_Timestamp("Bieg1_Gyroscope.csv",5)
+lista_akc_trucht = wczytaj_dane_z_pliku_csv_Timestamp("Trucht3_Accelerometer.csv",5)
+lista_gyr_trucht = wczytaj_dane_z_pliku_csv_Timestamp("Trucht3_Gyroscope.csv",5)
 
+usun_ostatnie_N_rekordow(lista_gyr_bieganie,1)
 dopasuj_rozmiar_listy(lista_akc_bieganie)
 dopasuj_rozmiar_listy(lista_gyr_bieganie)
 dopasuj_rozmiar_listy(lista_akc_trucht)
@@ -50,7 +69,7 @@ dopasuj_rozmiar_listy(lista_gyr_trucht)
 dane_ack = lista_akc_bieganie + lista_akc_trucht
 dane_gyr = lista_gyr_bieganie + lista_gyr_trucht
 
-IP_SERWERA = "192.168.8.106" # IP serwera - RPi
+IP_SERWERA = "192.168.8.102" # IP serwera
 PORT = 5005 # liczba 16-bitowa, wiekszta niz 1024
 BUFOR_ROZMIAR = 4096
 
@@ -61,24 +80,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 # SOCK_STREAM - TCP
     s.connect((IP_SERWERA,PORT))
     while True:
-        #s.send(input("Podaj wiadomosc do serwera: \n").encode())
         decyzja = input("Czy nadac porcje danych? T/N \n")
         if (decyzja == "T" or decyzja =="t" ) : decyzja = True
         else : decyzja = False
 
         if decyzja :
-            #do_nadania = " ".join(polacz_liste_w_1D(dane_ack[licznik * fs : ((licznik+1)*fs) ])) # nadaj porcje danych akcelerometr
-            #do_nadania = do_nadania + " "  + " ".join(polacz_liste_w_1D(dane_gyr[licznik * fs : ((licznik+1)*fs) ])) # nadaj porcje danych zyroskop
-            #print( len( dane_gyr[licznik * fs : ((licznik+1)*fs  ) ]  ))
-            #licznik = licznik + 1
-            #if licznik == (len(dane_ack)//fs) : licznik = 0
             probka_danych = []
             do_nadania = ""
             for i in range(fs * klasyfikuj_co_n_s):
                 probka_danych.append(dane_ack[i + (licznik * fs * klasyfikuj_co_n_s )])
                 probka_danych.append(dane_gyr[i + (licznik * fs * klasyfikuj_co_n_s )])
-                do_nadania = do_nadania + " " + " ".join(probka_danych[2*i]) + " " + " ".join(probka_danych[(2*i) + 1])
-                #print(do_nadania)
+                do_nadania = do_nadania + " " + " ".join(probka_danych[2*i]) + " " + " ".j$
+
             licznik = licznik+1
             if licznik == (len(dane_ack)//fs) : licznik = 0
             s.send(do_nadania.encode())
@@ -89,4 +102,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print("Odebrane dane to: \n",dane)
     
     #s.close()
+
 
