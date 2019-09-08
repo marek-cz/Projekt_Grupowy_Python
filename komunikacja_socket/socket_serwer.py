3d5efa2d27abc667dd17720cd55facfefb848c0a
Marek Czekala <m-czekala@o2.pl>
2019-09-08 08:02:11 +0200

    Dodanie analizy ramki

--- komunikacja_socket/socket_serwer.py
+++ komunikacja_socket/socket_serwer.py
@@ -33,11 +33,10 @@ BUFOR_ROZMIAR = 4096
 wykryta_aktywnosc = ["Marsz", "Trucht", "Bieg"]
 nazwa_pliku_z_baza_danych = "plik_bazy_danych.db"
 
-
+# polaczenie z baza danych SQLite
 polaczenie_z_baza_danych = sqlite3.connect(nazwa_pliku_z_baza_danych)
 kursor_bazy_danych = polaczenie_z_baza_danych.cursor() # za pomoca kursora wykonujemy operacje na bazie danych
-#kursor_bazy_danych.execute("create table probki (id integer primary key, a_x text, a_y text, a_z text, g_x text, g_y text, g_z text, czas datetime )")
-#polaczenie_z_baza_danych.commit()
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # AF_INET - IPv4
@@ -52,32 +51,42 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         data = connection.recv(BUFOR_ROZMIAR)
         if not data : break
-        dane = data.decode()
-        if dane =="END" : connection.send("END".encode())
-        #odpowiedz = "Tu serwer, odebralem dane :) "
-        #connection.send(odpowiedz.encode())
-        print('Odebralem dane : \n', data.decode())
+        ramka_danych_string = data.decode() # dane sa odbierane w formacie Byte
+        if ramka_danych_string =="END" : connection.send("END".encode())
+        print('Odebralem dane : \n', ramka_danych_string)
         print("\n\n")
-        dane_osie = wyodrebnij_osie_danych(dane.split())
-        probka = np.asarray(dane_osie).astype('float32')
-        shape = probka.shape
-        probka = probka.reshape(1,shape[0]*shape[1])
-        klasyfikacja = model.predict(probka)
-        indeks_np = np.where(klasyfikacja == klasyfikacja.max()) # szukamy najbardziej prawdopodobnego wyniku
-        indeks = np.asscalar(indeks_np[1])
-        # odeslij wynik klasyfikacji
-        odpowiedz = "Tu serwer, odebralem dane :)\nWykryta aktywnosc to :  " + wykryta_aktywnosc[indeks] + " \n"
-        connection.send(odpowiedz.encode())
 
-        kursor_bazy_danych.execute("insert into probki (a_x,a_y,a_z,g_x,g_y,g_z, etykieta ,czas) values(?,?,?,?,?,?,?,?)", (" ".join(dane_osie[0]), " ".join(dane_osie[1])," ".join(dane_osie[2]), " ".join(dane_osie[3])," ".join(dane_osie[4])," ".join(dane_osie[5]), wykryta_aktywnosc[indeks] ,datetime.datetime.now() ) )
-        polaczenie_z_baza_danych.commit()
+        ramka_danych_lista_stringow = ramka_danych_string.split()
+        # w zaleznosci od typu ramki - DO USTALENIA NA SPOTKANIU!!!! - realizujemy odpowiedznie operacje
+        
+        if ramka_danych_lista_stringow[0] == "Klasyfikacja" : # zakladana ramka : [Klasyfikacja][ID_ZAWODNIKA][DANE]
+            dane_osie = wyodrebnij_osie_danych(ramka_danych_lista_stringow[2:])
+            probka = np.asarray(dane_osie).astype('float32')
+            shape = probka.shape
+            probka = probka.reshape(1,shape[0]*shape[1])
+            klasyfikacja = model.predict(probka)
+            indeks_np = np.where(klasyfikacja == klasyfikacja.max()) # szukamy najbardziej prawdopodobnego wyniku
+            indeks = np.asscalar(indeks_np[1])
+            # odeslij wynik klasyfikacji
+            odpowiedz = "Tu serwer, odebralem dane :)\nWykryta aktywnosc to :  " + wykryta_aktywnosc[indeks] + " \n"
+            connection.send(odpowiedz.encode())
+            # wpisz do bazy danych
+            kursor_bazy_danych.execute("insert into probki (a_x,a_y,a_z,g_x,g_y,g_z, etykieta ,czas) values(?,?,?,?,?,?,?,?)", (" ".join(dane_osie[0]), " ".join(dane_osie[1])," ".join(dane_osie[2]), " ".join(dane_osie[3])," ".join(dane_osie[4])," ".join(dane_osie[5]), wykryta_aktywnosc[indeks] ,datetime.datetime.now() ) )
+            kursor_bazy_danych.execute("insert into aktywnosc (etykieta,czas,zawodnik_id) values(?,?,?)", (wykryta_aktywnosc[indeks],datetime.datetime.now(),int(ramka_danych_lista_stringow[1])) )
+            polaczenie_z_baza_danych.commit()
+        elif ramka_danych_lista_stringow[0] == "Wpis" : # wpisanie zawodnika [WPIS][IMIE][NAZWISKO][KLUB]
+            kursor_bazy_danych.execute("insert into zawodnik (imie,nazwisko,klub) values(?,?,?)", (ramka_danych_lista_stringow[1],ramka_danych_lista_stringow[2],ramka_danych_lista_stringow[3]) )
+        #elif ramka_danych_lista_stringow[0] == "Zapytanie" :
+            # kod obslugi zapytania
+            
+
 
     #connection.close()
 
 
-wynik = kursor_bazy_danych.execute("select * from probki")
+#wynik = kursor_bazy_danych.execute("select * from probki")
 
-print(wynik.fetchall()) # fetchall() - wybiera wszystkie rekordy | fetchone - 1 wiersz|fetchmany - kilka wierszy
+#print(wynik.fetchall()) # fetchall() - wybiera wszystkie rekordy | fetchone - 1 wiersz|fetchmany - kilka wierszy
 polaczenie_z_baza_danych.commit()
 polaczenie_z_baza_danych.close()
 
