64efb1a6375753a2e46f1f542baa6d4426ca3426
Marek Czekala <m-czekala@o2.pl>
2019-10-03 21:45:58 +0200

    Poprawa czytelnosci pliku.
    Usuniecie zbednej funkcji, wczytujacej dane bez sortowania
    po timestampie. Usuniecie zbednych zmiennych.
    Dodanie komentarzy do funkcji.
    Szczegoly zmian wyswietli git ;)

--- dane/przykladowe/funkcje.py
+++ dane/przykladowe/funkcje.py
@@ -3,36 +3,14 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-offset = 4
 
-normalizacja = True
 
 ##############################################################################################
-fs = 52 # Hz
-klasyfikuj_co_n_s = 1 # co ile chcemy klasyfikowac
-##############################################################################################
-#       ETYKIETY DANYCH :
-marsz = 0
-trucht = 1
-bieg = 2
-##############################################################################################
-def wczytaj_dane_z_pliku_csv(nazwa_pliku_csv, offset, etykieta ):
-    lista_na_dane = []
-    with open(nazwa_pliku_csv,"r") as plik :
-        licznik_linii = 0
-        for linia in plik:
-            licznik_linii += 1
-            if licznik_linii > offset :
-                linia_tokeny = linia.split(',') # dzielimy stringa po ','
-                wartosci = linia_tokeny[-4:-1]
-                wartosci.append(etykieta)
-                lista_na_dane.append(wartosci)# dane o ksztalcie (shape) np.(1243,3)
 
-    return lista_na_dane
-
-# with -> zapewnia zamkniecie pliku w przypadku bledu
-
-def wczytaj_dane_z_pliku_csv_Timestamp(nazwa_pliku_csv, offset, etykieta ):
+def wczytaj_dane_z_pliku_csv_Timestamp(nazwa_pliku_csv, etykieta, offset=4 ):
+    """ Wczytuje dane z pliku csv i sortuje je po timestampie
+        Parametr offset ma domyslna wartosc i okresla ile linii
+        poczatkowych pliku csv jets pomijanych."""
     lista_na_dane = []
     result = []
     with open(nazwa_pliku_csv,"r") as plik :
@@ -43,7 +21,7 @@ def wczytaj_dane_z_pliku_csv_Timestamp(nazwa_pliku_csv, offset, etykieta ):
                 linia_tokeny = linia.split(',') # dzielimy stringa po ','
                 wartosci = linia_tokeny[-6:-5] + linia_tokeny[-4:-1] # [-6] Node Timestamp
                 wartosci.append(etykieta)
-                lista_na_dane.append(wartosci)# dane o ksztalcie (shape) np.(1243,3)
+                lista_na_dane.append(wartosci)
         lista_na_dane.sort() # sortujemy po Timestamp'ie
         
         for i in range(len(lista_na_dane)): # usuwamy Timestamp z rezultatu
@@ -55,6 +33,10 @@ def wczytaj_dane_z_pliku_csv_Timestamp(nazwa_pliku_csv, offset, etykieta ):
 
 
 def wyodrebnij_os_z_tablicy(tablica, numer_osi):
+    """ Wybranie z listy danych o formacie :
+        [os_x os_y os_z, os_x os_y os_z, os_x os_y os_z,...]
+        jednej z osi. Zwraca liste w postaci np.:
+        [os_x0, os_x1, os_x2, ...]"""
     os = []
     licznik = 0
     for wiersz in tablica :
@@ -82,31 +64,38 @@ def usun_ostatnie_N_rekordow(lista,N):
         del(lista[(-1) * licznik])
         licznik += 1
 
-def dopasuj_rozmiar_listy(lista, fs = fs,klasyfikuj_co_n_s = klasyfikuj_co_n_s):
-    N = (klasyfikuj_co_n_s * fs) 
-    if len(lista) % N:
-        liczba_paczek = len(lista) // N
-        liczba_rekordow_do_usuniecia = len(lista) - (liczba_paczek * N)
+def dopasuj_rozmiar_listy(lista, liczba_probek_w_paczce):
+    """ Usuwa z listy tyle ostatnich pozycji aby jej dlugosc byla wielokrotnoscia
+        liczby probek w paczce danych, ktora wchodzi na wejscie klasyfikatora"""
+    if len(lista) % liczba_probek_w_paczce:
+        liczba_paczek = len(lista) // liczba_probek_w_paczce
+        liczba_rekordow_do_usuniecia = len(lista) - (liczba_paczek * liczba_probek_w_paczce)
         usun_ostatnie_N_rekordow(lista,liczba_rekordow_do_usuniecia)
 
 def ustaw_w_losowej_kolejnosci(kolejnosc,tablica_oryginalna, tablica_docelowa,liczba_probek):
+    """ Ustawia tablica_docelowa wedlug kolejnosci podanej w liscie kolejnosc"""
     licznik = 0
     while(licznik < liczba_probek):
         tablica_docelowa[licznik] = tablica_oryginalna[kolejnosc[licznik]]
         licznik += 1
 
-def wyznacz_etykiete(etykieta_pliku):
+def wyznacz_etykiete(etykieta_pliku, slownik_etykiet_danych ):
+    """ Na podstawie etykiety pliku (fragment nazwy pliku
+        np. Bieg w Bieg1_Accelerometer.csv ) wyznacza ety-
+        kiete danych. Zwraca -1 gdy blad."""
     if ( etykieta_pliku.find("Bieg") !=(-1) ):
-        return bieg
+        return slownik_etykiet_danych["Bieg"]
     elif (etykieta_pliku.find("Marsz") !=(-1)):
-        return marsz
+        return slownik_etykiet_danych["Marsz"]
     elif ( etykieta_pliku.find("Trucht") !=(-1) ):
-        return trucht
+        return slownik_etykiet_danych["Trucht"]
     else :
         return -1 # blad
 
 
-def wczytaj_dane_z_plikow():
+def wczytaj_dane_z_plikow(liczba_probek_w_paczce, slownik_etykiet_danych):
+    """ Wczytuje dane z WSZYSTKICH plikow csv znajdujacych sie
+        w tym samym katalogu."""
     import os # biblioteka do poruszania sie w systemie plikow
     lista_przetworzonych_plikow = [] # lista na nazwy plikow ktore byly juz przetwarzane
     oznaczenie_gyr = "_Gyroscope.csv"
@@ -121,14 +110,14 @@ def wczytaj_dane_z_plikow():
                 nazwa_pliku_tokeny = plik.name.split('_') # dzielimy po _
                 etykieta_pliku = nazwa_pliku_tokeny[0]
                 nazwa_pliku_gyr = etykieta_pliku + oznaczenie_gyr
-                etykieta = wyznacz_etykiete(etykieta_pliku)
+                etykieta = wyznacz_etykiete(etykieta_pliku, slownik_etykiet_danych)
                 if ( etykieta != (-1) ): # jezeli nazwa pliku zawiera etykiete danych
                     # WCZYTAJ DANE
-                    lista_tmp_akc = wczytaj_dane_z_pliku_csv_Timestamp(plik.name,offset,etykieta)
-                    lista_tmp_gyr = wczytaj_dane_z_pliku_csv_Timestamp(nazwa_pliku_gyr,offset,etykieta)
+                    lista_tmp_akc = wczytaj_dane_z_pliku_csv_Timestamp(plik.name,etykieta)
+                    lista_tmp_gyr = wczytaj_dane_z_pliku_csv_Timestamp(nazwa_pliku_gyr,etykieta)
                     # DOPASUJ ROZMIAR
-                    dopasuj_rozmiar_listy(lista_tmp_akc)
-                    dopasuj_rozmiar_listy(lista_tmp_gyr)
+                    dopasuj_rozmiar_listy(lista_tmp_akc,liczba_probek_w_paczce)
+                    dopasuj_rozmiar_listy(lista_tmp_gyr,liczba_probek_w_paczce)
                     # DODAJ DO BUFORA
                     lista_akc += lista_tmp_akc
                     lista_gyr += lista_tmp_gyr
