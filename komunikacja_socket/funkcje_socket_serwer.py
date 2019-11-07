9f3aa06cc07bb876c26fe0b49cd8b2c0e20ec657
Marek Czekala <m-czekala@o2.pl>
2019-11-07 17:39:30 +0100

    Modul zawierajacy stale i funkcje
    Zawiera stale i funkcje wykorzystywane w skryptach
    serwerow.

---
+++ komunikacja_socket/funkcje_socket_serwer.py
@@ -0,0 +1,153 @@
+""" MODUL ZAWIERAJACY FUNKCJE I STALE WYKORZYSTYWANE W SKRYPTACH
+    KLASYFIKUJACYCH DANE"""
+
+import numpy as np
+
+###############################################################################################
+#                                       STALE
+###############################################################################################
+ZAKRES_AKCELEROMETR = 8000
+ZAKRES_ZYROSKOP     = 2000
+
+IP_SERWERA = '' # U serwera puste
+PORT = 5005 # liczba 16-bitowa, wiekszta niz 1024
+BUFOR_ROZMIAR = 4096
+
+
+BIEGANIE_PROGI_ODCHYLENIA_STD = {"akc_x" : 2000, "akc_y" : 2000, "akc_z" : 2000, "gyr_x" : 2000, "gyr_y" : 300, "gyr_z" : 2000 }
+MARSZ_PROGI_ODCHYLENIA_STD    = {"akc_x" : 500,  "akc_y" : 2000, "akc_z" : 500,  "gyr_x" : 2000, "gyr_y" : 10, "gyr_z" : 2000 }
+TRUCHT_PROGI_ODCHYLENIA_STD   = {"akc_x" : 1300, "akc_y" : 2000, "akc_z" : 1300, "gyr_x" : 2000, "gyr_y"  : 10, "gyr_z" : 2000 }
+
+PROGI_ODCHYLENIE_STD = {"Bieganie" : BIEGANIE_PROGI_ODCHYLENIA_STD, "Marsz" : MARSZ_PROGI_ODCHYLENIA_STD, "Trucht" : TRUCHT_PROGI_ODCHYLENIA_STD } 
+
+
+
+wykryta_aktywnosc = ["Marsz", "Trucht", "Bieg","Stanie"]
+nazwa_pliku_z_baza_danych = "plik_bazy_danych.db"
+sciezka_do_modelu_klasyfikatora = "../zapis_modelu.h5"
+
+
+###############################################################################################
+#                                       FUNKCJE
+###############################################################################################
+def wyodrebnij_osie_danych_BEZ_NORMALIZACJI(lista): # [DANE] = [a_x][a_y][a_z][g_x][g_y][g_z] * Liczba_probek_w_paczce
+    result = [[],[],[],[],[],[]]
+    for probka in lista:
+        result[0].append(probka[0] ) # AKCELEROMETR X
+        result[1].append(probka[1] ) # AKCELEROMETR Y
+        result[2].append(probka[2] ) # AKCELEROMETR Z
+        result[3].append(probka[3] ) # ZYROSKOP X
+        result[4].append(probka[4] ) # ZYROSKOP Y
+        result[5].append(probka[5] ) # ZYROSKOP Z
+
+    return result
+
+def wyodrebnij_osie_danych_NORMALIZACJA(lista): # [DANE] = [a_x][a_y][a_z][g_x][g_y][g_z] * Liczba_probek_w_paczce
+    """ WYODREBNIA OSIE DANYCH I NORMALIZUJE WZGLEDEM
+        ZAKRESOW POMIAROWYCH CZUJNIKOW - KLASYFIKATOR
+        NEURONOWY"""
+    result = [[],[],[],[],[],[]]
+    for probka in lista:
+        result[0].append(probka[0] / ZAKRES_AKCELEROMETR ) # AKCELEROMETR X + NORMALIZACJA WZGLEDEM ZAKRESU
+        result[1].append(probka[1] / ZAKRES_AKCELEROMETR ) # AKCELEROMETR Y
+        result[2].append(probka[2] / ZAKRES_AKCELEROMETR ) # AKCELEROMETR Z
+        result[3].append(probka[3] / ZAKRES_ZYROSKOP ) # ZYROSKOP X
+        result[4].append(probka[4] / ZAKRES_ZYROSKOP ) # ZYROSKOP Y
+        result[5].append(probka[5] / ZAKRES_ZYROSKOP ) # ZYROSKOP Z
+
+    return result
+
+
+def wyodrebnij_osie_danych_string(lista):
+    result = [[],[],[],[],[],[]]
+    for probka in lista:
+        result[0].append(probka[0]) # AKCELEROMETR X
+        result[1].append(probka[1]) # AKCELEROMETR Y
+        result[2].append(probka[2]) # AKCELEROMETR Z
+        result[3].append(probka[3]) # ZYROSKOP X
+        result[4].append(probka[4]) # ZYROSKOP Y
+        result[5].append(probka[5]) # ZYROSKOP Z
+
+    return result
+
+
+def normalizuj(x): # dokladniej standaryzacja - wartosc srednia 0 i odchylenie standardowe 1
+    srednie = x.mean(axis=1)
+    for i in range(x.shape[0]):
+        x[i] -= srednie[i]
+    odchylenia_std = x.std(axis=1)
+    for i in range(x.shape[0]):
+        x[i] /= odchylenia_std[i]
+
+def lista_stringow_na_liste_probek(lista_stringow):
+    result = []
+    rekord = []
+    licznik = 0
+    for string in lista_stringow:
+        #napis = str(licznik) + ". String : " + str(string)
+        rekord.append(string)
+        #print(napis)
+        licznik += 1
+        if licznik == 7 :
+            licznik = 0
+            result.append(rekord)
+            rekord = []
+            #print(result)
+
+    return result
+
+def usun_timestamp(lista_probek_timestamp):
+    result = []
+    for probka in lista_probek_timestamp:
+        result.append(probka[1:])
+    return result
+
+
+def klasyfikacja_odchylenie_std(probka_danych):
+    odchylenia_std_osi = []
+    for os in probka_danych:
+        odchylenia_std_osi.append(os.std())
+    if (odchylenia_std_osi[0] >= PROGI_ODCHYLENIE_STD['Bieganie']['akc_x']) and (odchylenia_std_osi[2] >= PROGI_ODCHYLENIE_STD['Bieganie']['akc_z']) and (odchylenia_std_osi[4] >= PROGI_ODCHYLENIE_STD['Bieganie']['gyr_y']):
+        return 2 # indeks biegania
+    elif (odchylenia_std_osi[0] >= PROGI_ODCHYLENIE_STD['Trucht']['akc_x']) and (odchylenia_std_osi[2] >= PROGI_ODCHYLENIE_STD['Trucht']['akc_z']) and (odchylenia_std_osi[4] >= PROGI_ODCHYLENIE_STD['Trucht']['gyr_y']):
+        return 1 # indeks truchtu
+    elif (odchylenia_std_osi[0] >= PROGI_ODCHYLENIE_STD['Marsz']['akc_x']) and (odchylenia_std_osi[2] >= PROGI_ODCHYLENIE_STD['Marsz']['akc_z']) and (odchylenia_std_osi[4] >= PROGI_ODCHYLENIE_STD['Marsz']['gyr_y']):
+        return 1 # indeks marszu
+    else :
+        return 3 # indeks stania
+
+
+def lista_stringow_na_probke(lista_danych):
+    """ Na podstawie listy stringow formuje probke danych do modelu
+        sieci neuronowej. Zwraca probke (macierz numpy) i dane z
+        czujnikow w formie listy stringow"""
+    lista_probek_timestamp = lista_stringow_na_liste_probek(lista_danych)
+    lista_probek_timestamp.sort() # sortujemy po Timestamp
+    lista_posortowanych_probek = usun_timestamp(lista_probek_timestamp) # usuwamy timestamp
+    lista_posortowanych_probek_float = np.asarray(lista_posortowanych_probek).astype('float32') # przechodzimy na floaty
+    dane_osie = wyodrebnij_osie_danych_NORMALIZACJA(lista_posortowanych_probek_float) # podzial probek na osie czujnikow
+    probka = np.asarray(dane_osie).astype('float32')
+    shape = probka.shape
+    probka = probka.reshape(1,shape[0]*shape[1])
+
+    dane_osie_string = wyodrebnij_osie_danych_string(lista_posortowanych_probek) # podzial probek na osie czujnikow - do zapisu
+
+    return (probka,dane_osie_string)
+
+def lista_stringow_na_probke_BEZ_NORMALIZACJI(lista_danych):
+    """ Na podstawie listy stringow formuje probke danych do modelu
+        sieci neuronowej. Zwraca probke (macierz numpy) i dane z
+        czujnikow w formie listy stringow"""
+    lista_probek_timestamp = lista_stringow_na_liste_probek(lista_danych)
+    lista_probek_timestamp.sort() # sortujemy po Timestamp
+    lista_posortowanych_probek = usun_timestamp(lista_probek_timestamp) # usuwamy timestamp
+    lista_posortowanych_probek_float = np.asarray(lista_posortowanych_probek).astype('float32') # przechodzimy na floaty
+    dane_osie = wyodrebnij_osie_danych_BEZ_NORMALIZACJI(lista_posortowanych_probek_float) # podzial probek na osie czujnikow
+    probka = np.asarray(dane_osie).astype('float32')
+    #print(probka)
+
+    dane_osie_string = wyodrebnij_osie_danych_string(lista_posortowanych_probek) # podzial probek na osie czujnikow - do zapisu
+
+    return (probka,dane_osie_string)
+
+#####################################################################################
