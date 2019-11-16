c33342874c76716ce92c2e3a74f2c750332d3adf
Marek Czekala <m-czekala@o2.pl>
2019-11-16 18:29:42 +0100

    Modyfikacje - blad do bazy danych
    Modyfikacje funkcji w zwiazku z dodaniem do skryptu
    socket_server.py mozliwosci dodawania blednych paczek
    danych do bazy danych

--- komunikacja_socket/funkcje_socket_serwer.py
+++ komunikacja_socket/funkcje_socket_serwer.py
@@ -15,8 +15,8 @@ BUFOR_ROZMIAR = 4096
 
 
 BIEGANIE_PROGI_ODCHYLENIA_STD = {"akc_x" : 2000, "akc_y" : 2000, "akc_z" : 2000, "gyr_x" : 2000, "gyr_y" : 300, "gyr_z" : 2000 }
-MARSZ_PROGI_ODCHYLENIA_STD    = {"akc_x" : 500,  "akc_y" : 2000, "akc_z" : 500,  "gyr_x" : 2000, "gyr_y" : 10, "gyr_z" : 2000 }
-TRUCHT_PROGI_ODCHYLENIA_STD   = {"akc_x" : 1300, "akc_y" : 2000, "akc_z" : 1300, "gyr_x" : 2000, "gyr_y"  : 10, "gyr_z" : 2000 }
+MARSZ_PROGI_ODCHYLENIA_STD    = {"akc_x" : 500,  "akc_y" : 2000, "akc_z" : 500,  "gyr_x" : 2000, "gyr_y" : 100, "gyr_z" : 2000 }
+TRUCHT_PROGI_ODCHYLENIA_STD   = {"akc_x" : 1300, "akc_y" : 2000, "akc_z" : 1300, "gyr_x" : 2000, "gyr_y"  : 200, "gyr_z" : 2000 }
 
 PROGI_ODCHYLENIE_STD = {"Bieganie" : BIEGANIE_PROGI_ODCHYLENIA_STD, "Marsz" : MARSZ_PROGI_ODCHYLENIA_STD, "Trucht" : TRUCHT_PROGI_ODCHYLENIA_STD } 
 
@@ -107,41 +107,49 @@ def klasyfikacja_odchylenie_std(probka_danych):
     odchylenia_std_osi = []
     for os in probka_danych:
         odchylenia_std_osi.append(os.std())
-    if (odchylenia_std_osi[0] >= PROGI_ODCHYLENIE_STD['Bieganie']['akc_x']) and (odchylenia_std_osi[2] >= PROGI_ODCHYLENIE_STD['Bieganie']['akc_z']) and (odchylenia_std_osi[4] >= PROGI_ODCHYLENIE_STD['Bieganie']['gyr_y']):
+        print(odchylenia_std_osi)
+    if (odchylenia_std_osi[0] >= PROGI_ODCHYLENIE_STD['Bieganie']['akc_x']) or (odchylenia_std_osi[2] >= PROGI_ODCHYLENIE_STD['Bieganie']['akc_z']) or (odchylenia_std_osi[4] >= PROGI_ODCHYLENIE_STD['Bieganie']['gyr_y']):
         return 2 # indeks biegania
-    elif (odchylenia_std_osi[0] >= PROGI_ODCHYLENIE_STD['Trucht']['akc_x']) and (odchylenia_std_osi[2] >= PROGI_ODCHYLENIE_STD['Trucht']['akc_z']) and (odchylenia_std_osi[4] >= PROGI_ODCHYLENIE_STD['Trucht']['gyr_y']):
+    elif (odchylenia_std_osi[0] >= PROGI_ODCHYLENIE_STD['Trucht']['akc_x']) or (odchylenia_std_osi[2] >= PROGI_ODCHYLENIE_STD['Trucht']['akc_z']) or (odchylenia_std_osi[4] >= PROGI_ODCHYLENIE_STD['Trucht']['gyr_y']):
         return 1 # indeks truchtu
-    elif (odchylenia_std_osi[0] >= PROGI_ODCHYLENIE_STD['Marsz']['akc_x']) and (odchylenia_std_osi[2] >= PROGI_ODCHYLENIE_STD['Marsz']['akc_z']) and (odchylenia_std_osi[4] >= PROGI_ODCHYLENIE_STD['Marsz']['gyr_y']):
+    elif (odchylenia_std_osi[0] >= PROGI_ODCHYLENIE_STD['Marsz']['akc_x']) or (odchylenia_std_osi[2] >= PROGI_ODCHYLENIE_STD['Marsz']['akc_z']) or (odchylenia_std_osi[4] >= PROGI_ODCHYLENIE_STD['Marsz']['gyr_y']):
         return 1 # indeks marszu
     else :
         return 3 # indeks stania
 
 
-def lista_stringow_na_probke(lista_danych):
+def lista_stringow_na_probke(lista_danych, zamien_na_float = True):
     """ Na podstawie listy stringow formuje probke danych do modelu
         sieci neuronowej. Zwraca probke (macierz numpy) i dane z
         czujnikow w formie listy stringow"""
     lista_probek_timestamp = lista_stringow_na_liste_probek(lista_danych)
     lista_probek_timestamp.sort() # sortujemy po Timestamp
     lista_posortowanych_probek = usun_timestamp(lista_probek_timestamp) # usuwamy timestamp
-    lista_posortowanych_probek_float = np.asarray(lista_posortowanych_probek).astype('float32') # przechodzimy na floaty
-    dane_osie = wyodrebnij_osie_danych_NORMALIZACJA(lista_posortowanych_probek_float) # podzial probek na osie czujnikow
-    probka = np.asarray(dane_osie).astype('float32')
-    shape = probka.shape
-    probka = probka.reshape(1,shape[0]*shape[1])
-
     dane_osie_string = wyodrebnij_osie_danych_string(lista_posortowanych_probek) # podzial probek na osie czujnikow - do zapisu
+    if zamien_na_float:
+        #try :
+        lista_posortowanych_probek_float = np.asarray(lista_posortowanych_probek).astype('float32') # przechodzimy na floaty
+        #except ValueError as zmienna1:
+        #    print("Blad konwersji  string - float!")
+        dane_osie = wyodrebnij_osie_danych_NORMALIZACJA(lista_posortowanych_probek_float) # podzial probek na osie czujnikow
+        probka = np.asarray(dane_osie).astype('float32')
+        shape = probka.shape
+        probka = probka.reshape(1,shape[0]*shape[1])
+    else :
+        probka = "Blad"
+
 
     return (probka,dane_osie_string)
 
 def lista_stringow_na_probke_BEZ_NORMALIZACJI(lista_danych):
-    """ Na podstawie listy stringow formuje probke danych do modelu
-        sieci neuronowej. Zwraca probke (macierz numpy) i dane z
-        czujnikow w formie listy stringow"""
     lista_probek_timestamp = lista_stringow_na_liste_probek(lista_danych)
     lista_probek_timestamp.sort() # sortujemy po Timestamp
     lista_posortowanych_probek = usun_timestamp(lista_probek_timestamp) # usuwamy timestamp
-    lista_posortowanych_probek_float = np.asarray(lista_posortowanych_probek).astype('float32') # przechodzimy na floaty
+    try :
+        lista_posortowanych_probek_float = np.asarray(lista_posortowanych_probek).astype('float32') # przechodzimy na floaty
+    except ValueError as zmienna1:
+        print("Blad konwersji  string - float!\n")
+        print(lista_posortowanych_probek)
     dane_osie = wyodrebnij_osie_danych_BEZ_NORMALIZACJI(lista_posortowanych_probek_float) # podzial probek na osie czujnikow
     probka = np.asarray(dane_osie).astype('float32')
     #print(probka)
