24784ad292024898c49ff3cd938d8968ccb06195
Marek Czekala <m-czekala@o2.pl>
2019-11-24 11:33:34 +0100

    Usuniecie \n\n z echa

--- komunikacja_socket/serwer_skrypt.sh
+++ komunikacja_socket/serwer_skrypt.sh
@@ -3,16 +3,16 @@
 while true
 do
 	python3  socket_serwer.py 
-	echo "\n\nAby wyjsc CTRL + C"
+	echo "Aby wyjsc CTRL + C"
 	sleep 5
 	kill $(lsof -t -i:5005)
-	echo "\n\nAby wyjsc CTRL + C"
+	echo "Aby wyjsc CTRL + C"
 	sleep 5
 	kill $(lsof -t -i:5005)
-	echo "\n\nAby wyjsc CTRL + C"
+	echo "Aby wyjsc CTRL + C"
 	sleep 5
 	kill $(lsof -t -i:5005)
-	echo "\n\nAby wyjsc CTRL + C"
+	echo "Aby wyjsc CTRL + C"
 	sleep 5
 	kill $(lsof -t -i:5005)
 
